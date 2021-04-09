9f3c123d6ce8e4843f9b28d9fd158d49248621f5
Venkatesh-R-Nayak <81382527+Venkatesh-R-Nayak@users.noreply.github.com>
2021-04-09 22:40:18 +0530

    Uploaded springBoot files

---
+++ SpringBoot-App/src/main/java/com/ltts/ReactSpringSkywiseApplication.java
@@ -0,0 +1,13 @@
+package com.ltts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringSkywiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringSkywiseApplication.class, args);
+	}
+
+}
